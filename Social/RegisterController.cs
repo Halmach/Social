c7d5c86ef2aadbef70c099d7be68dc44e2a20bc5
Khalmatov <large000@yandex.ru>
2022-07-12 21:35:11 +0300

    Добавьте файлы проекта.

---
+++ Social/RegisterController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Social
+{
+    public class RegisterController:Controller
+    {
+
+        [Route("Register")]
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View("Home/Register");
+        }
+
+        [Route("RegisterPart2")]
+        [HttpGet]
+        public IActionResult RegisterPart2(RegisterViewModel model)
+        {
+            return View("RegisterPart2", model);
+        }
+    }
+}
